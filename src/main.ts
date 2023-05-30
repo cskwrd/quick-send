9e792c440016d6eed6921b69968b56799677867b
cskwrd <cskwrd@users.noreply.github.com>
2023-05-29 21:12:02 -0400

    Update main to call smtp lib

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+// import {wait} from './wait'
+import {smtp} from './smtp'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const remoteHost: string = core.getInput('remote-host')
+    const remotePort: string = core.getInput('remote-port')
+    core.debug(`Connecting to '${remoteHost}' on port '${remotePort}' ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
+    await smtp(remoteHost, parseInt(remotePort, 10))
     core.debug(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
